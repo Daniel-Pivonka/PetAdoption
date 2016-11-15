b09e3c1577419a71f515dcab98c6cca54bd05dc7
DAN\dpivonka <dpivonka@comcast.net>
2016-11-15 16:44:16 -0500

    some stuff

---
+++ app/src/main/java/com/uml/dpivonka/petadoption/Pets.java
@@ -0,0 +1,20 @@
+package com.uml.dpivonka.petadoption;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dpivonka on 11/15/2016.
+ */
+public class Pets {
+    String Id;
+    String Shelter;
+    String Name;
+    String Animal;
+    String Breed;
+    String Age;
+    String Size;
+    String Options;
+    String Description;
+    ArrayList<String> PhotoUrl;
+    String Contact;
+}
---
+++ app/src/main/java/com/uml/dpivonka/petadoption/Utils.java
@@ -0,0 +1,16 @@
+package com.uml.dpivonka.petadoption;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by dpivonka on 11/15/2016.
+ */
+
+public class Utils {
+    public static ArrayList<Pets> fetchAnimalData() {
+        return null;
+    }
+
+
+}
