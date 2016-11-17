9a03d23abc46dfbf1c5fa0f65e43ddc14fcd5382
DAN\dpivonka <dpivonka@comcast.net>
2016-11-17 12:16:08 -0500

    dan stuff

--- app/src/main/java/com/uml/dpivonka/petadoption/Pets.java
+++ app/src/main/java/com/uml/dpivonka/petadoption/Pets.java
@@ -6,15 +6,27 @@ import java.util.ArrayList;
  * Created by dpivonka on 11/15/2016.
  */
 public class Pets {
-    String Id;
-    String Shelter;
-    String Name;
-    String Animal;
-    String Breed;
-    String Age;
-    String Size;
-    String Options;
-    String Description;
-    ArrayList<String> PhotoUrl;
-    String Contact;
+    private String Name;
+    private String Sex;
+    private String Animal;
+    private String Breed;
+    private String Age;
+    private String Size;
+    private ArrayList<String> Options;
+    private String Description;
+    private ArrayList<String> PhotoUrl;
+    private String Contact;
+
+    Pets(String name, String sex, String animal, String breed, String age, String size, ArrayList<String> options, String description, ArrayList<String> photoUrl, String contact) {
+        Name = name;
+        Sex = sex;
+        Animal = animal;
+        Breed = breed;
+        Age = age;
+        Size = size;
+        Options = options;
+        Description = description;
+        PhotoUrl = photoUrl;
+        Contact = contact;
+    }
 }
--- app/src/main/java/com/uml/dpivonka/petadoption/Utils.java
+++ app/src/main/java/com/uml/dpivonka/petadoption/Utils.java
@@ -1,11 +1,21 @@
 package com.uml.dpivonka.petadoption;
 
 
+import android.text.TextUtils;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 /**
@@ -13,16 +23,56 @@ import java.util.ArrayList;
  */
 
 public class Utils {
+
+    public static final String LOG_TAG = Utils.class.getSimpleName();
+
     public static ArrayList<Pets> fetchAnimalData(ArrayList<String> preferences) {
+
+        String url = "http://api.petfinder.com/pet.find?key=0657eb03f8e01bbe903b5adacdd5bf8c&format=json";
+
+        //adding preferences
+        url += "&animal=dog&animal=cat&location=lowell,%20Massachusetts";
 
         String jsonResponse = null;
         try {
-            jsonResponse = makeHttpRequest(url);
+            jsonResponse = makeHttpRequest(createUrl(url));
         } catch (IOException e) {
 
         }
 
-        makeHttpRequest(createUrl("http://api.petfinder.com/pet.find?key=0657eb03f8e01bbe903b5adacdd5bf8c&animal=dog&animal=cat&location=lowell,%20Massachusetts"));
+        return extractDataFromJson(jsonResponse);
+    }
+
+    private static ArrayList<Pets> extractDataFromJson(String jsonResponse) {
+        ArrayList<Pets> pets = new ArrayList<Pets>();
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(jsonResponse)) {
+            return null;
+        }
+
+        try {
+            JSONObject baseJsonResponse = new JSONObject(jsonResponse);
+            baseJsonResponse = baseJsonResponse.getJSONObject("petfinder");
+            baseJsonResponse = baseJsonResponse.getJSONObject("pets");
+            JSONArray jsonPetArray = baseJsonResponse.getJSONArray("pet");
+
+            for(int x = 0; x < jsonPetArray.length(); x++) {
+                // Extract out the pet
+                JSONObject jsonPet = jsonPetArray.getJSONObject(x);
+
+                //creat new pet
+                Pets pet = new Pets();
+
+                //fill pet with info from json
+
+                pets.add(pet);
+            }
+
+
+            return pets;
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+        }
         return null;
     }
 
@@ -31,12 +81,59 @@ public class Utils {
         try {
             url = new URL(stringUrl);
         } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL ", e);
         }
         return url;
     }
 
     private static String makeHttpRequest(URL url) throws IOException {
         String jsonResponse = "";
+
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
         return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
     }
 }
