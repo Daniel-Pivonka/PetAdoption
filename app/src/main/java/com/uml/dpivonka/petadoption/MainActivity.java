acca15799390cb8396c35a1092b862d794e489cf
Brianne Bindas <briannebindas@gmail.com>
2016-11-22 00:50:53 -0500

    options for pets, error for invalid location

--- app/src/main/java/com/uml/dpivonka/petadoption/MainActivity.java
+++ app/src/main/java/com/uml/dpivonka/petadoption/MainActivity.java
@@ -1,5 +1,7 @@
 package com.uml.dpivonka.petadoption;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,37 +10,49 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addListenerOnSpinnerItemSelection();
+
         //get submit button
         final Button button = (Button) findViewById(R.id.button2);
         //get zip and city,state text fields
         final EditText zip = (EditText) findViewById(R.id.postal_field);
         final EditText city = (EditText) findViewById(R.id.city_field);
-        final EditText state = (EditText) findViewById(R.id.state_field);
 
         //if button is clicked
         button.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent petTypeIntent = new Intent(MainActivity.this, PetTypeActivity.class);
+
+
                 //if valid zipcode
                 if(zip.getText().toString().matches("^\\d{5}(?:[-\\s]\\d{4})?$")) {
                     startAysnc(zip.getText().toString());
-                } else if (!city.getText().toString().isEmpty() && !state.getText().toString().isEmpty()) {
-                    startAysnc(city.getText().toString() + ",%20" + state.getText().toString());
+                    startActivity(petTypeIntent);
+                } else if (!city.getText().toString().isEmpty() && !(String.valueOf(spinner1.getSelectedItem()) == "")) {
+                    startAysnc(city.getText().toString() + ",%20" + String.valueOf(spinner1.getSelectedItem()));
+                    startActivity(petTypeIntent);
                 } else {
-                    button.setText("invalid");
+                    Toast.makeText(getApplicationContext(),
+                            "Invalid location submission.", Toast.LENGTH_LONG).show();
+                    button.setTextColor(Color.RED);
                 }
-
             }
         });
 
@@ -71,5 +85,9 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(pets.get(x).getName());
             }
         }
+    }
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.state_field);
     }
 }
---
+++ app/src/main/java/com/uml/dpivonka/petadoption/PetTypeActivity.java
@@ -0,0 +1,15 @@
+package com.uml.dpivonka.petadoption;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bbindas on 11/21/16.
+ */
+public class PetTypeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_type);
+    }
+}
