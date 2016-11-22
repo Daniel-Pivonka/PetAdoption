cc26295fc07dac4b20733d54878dfba1c37972e2
DAN\dpivonka <dpivonka@comcast.net>
2016-11-22 16:21:20 -0500

    moved asynctask

--- app/src/main/java/com/uml/dpivonka/petadoption/MainActivity.java
+++ app/src/main/java/com/uml/dpivonka/petadoption/MainActivity.java
@@ -2,14 +2,11 @@ package com.uml.dpivonka.petadoption;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -40,13 +37,12 @@ public class MainActivity extends AppCompatActivity {
                 // Create a new intent to open the {@link NumbersActivity}
                 Intent petTypeIntent = new Intent(MainActivity.this, PetTypeActivity.class);
 
-
                 //if valid zipcode
                 if(zip.getText().toString().matches("^\\d{5}(?:[-\\s]\\d{4})?$")) {
-                    startAysnc(zip.getText().toString());
+                    petTypeIntent.putExtra("location", zip.getText().toString());
                     startActivity(petTypeIntent);
                 } else if (!city.getText().toString().isEmpty() && !(String.valueOf(spinner1.getSelectedItem()) == "")) {
-                    startAysnc(city.getText().toString() + ",%20" + String.valueOf(spinner1.getSelectedItem()));
+                    petTypeIntent.putExtra("location", city.getText().toString() + ",%20" + String.valueOf(spinner1.getSelectedItem()));
                     startActivity(petTypeIntent);
                 } else {
                     Toast.makeText(getApplicationContext(),
@@ -56,35 +52,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    public void startAysnc(String location){
-        System.out.println(location);
-        ArrayList<String> preferences = new ArrayList<String>();
-        preferences.add(location);
-        PetAsyncTask task = new PetAsyncTask();
-        task.execute(preferences);
-    }
-
-    public class PetAsyncTask extends AsyncTask<ArrayList<String>, Void, ArrayList<Pets>>
-    {
-
-        @Override
-        protected ArrayList<Pets> doInBackground(ArrayList<String>... params) {
-            ArrayList<String> preferences = null;
-            for (ArrayList<String> item : params) {
-                preferences = item;
-            }
-            return Utils.fetchAnimalData(preferences);
-        }
-
-        protected void onPostExecute(ArrayList<Pets> pets) {
-
-            //scott take this array of pets and fill the sql database with it
-            for (int x = 0; x < pets.size(); x++) {
-                System.out.println(pets.get(x).getName());
-            }
-        }
     }
 
     public void addListenerOnSpinnerItemSelection() {
