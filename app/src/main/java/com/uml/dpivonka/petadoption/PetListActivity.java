022fcb6ef41264fe77494a557ba7341f3cc44ce1
Brianne Bindas <briannebindas@gmail.com>
2016-11-29 12:31:12 -0500

    added check stuff

--- app/src/main/java/com/uml/dpivonka/petadoption/PetListActivity.java
+++ app/src/main/java/com/uml/dpivonka/petadoption/PetListActivity.java
@@ -1,8 +1,19 @@
 package com.uml.dpivonka.petadoption;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
 /**
  * Created by bbindas on 11/28/16.
  */
-public class PetListActivity {
-    
+public class PetListActivity extends AppCompatActivity {
+    // this will be used to display the list of pet options
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+
+
+    }
 }
--- app/src/main/java/com/uml/dpivonka/petadoption/PetTypeActivity.java
+++ app/src/main/java/com/uml/dpivonka/petadoption/PetTypeActivity.java
@@ -21,11 +21,6 @@ public class PetTypeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_type);
 
-<<<<<<< HEAD
-        final Button button = (Button) findViewById(R.id.button3);
-=======
->>>>>>> 1f08b3aa14f7b83b679f68c2f5f6096a99c9f362
-
         Intent intent = getIntent();
         final String location = intent.getStringExtra("location");
 
@@ -34,11 +29,16 @@ public class PetTypeActivity extends AppCompatActivity {
         {
             public void onClick(View v)
             {
-                // Create a new intent to open the {@link NumbersActivity}
-                Intent petTypeIntent = new Intent(PetTypeActivity.this, PetListActivity.class);
-                ArrayList<String> checkedSpecs = onCheckboxClicked(v);
-                checkedSpecs.add(location);
+                // Create a new intent to open the {@link PetListActivity}
+
+                //Intent petListIntent = new Intent(PetTypeActivity.this, PetListActivity.class);
+                if(v != findViewById(R.id.button3)) {
+                    ArrayList<String> checkedSpecs = onCheckboxClicked(v);
+                    checkedSpecs.add(location);
+                }
                 startAysnc(location); // change this to take an ArrayList<String> -> checkSpecs
+                Intent intent = new Intent(PetTypeActivity.this, PetListActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -78,224 +78,95 @@ public class PetTypeActivity extends AppCompatActivity {
 
     }
 
-<<<<<<< HEAD
-    public ArrayList<String> onCheckboxClicked(View view) {
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
+    public ArrayList<String> submitPreferences(View view) {
         ArrayList<String> petSpecs = new ArrayList<String>();
 
-        // Check which checkbox was clicked
-        switch(view.getId()) {
-            case R.id.barnyard:
-                if (checked)
-                    petSpecs.add("barnyard");
-                else {
-                    if (!petSpecs.contains("barnyard"))
-                        break;
-                    else
-                        petSpecs.remove("barnyard");
-                }
-                break;
-            case R.id.bird:
-                if (checked)
-                    petSpecs.add("bird");
-                else {
-                    if (!petSpecs.contains("bird"))
-                        break;
-                    else
-                        petSpecs.remove("bird");
-                }
-                break;
-            case R.id.cat:
-                if (checked)
-                    petSpecs.add("cat");
-                else {
-                    if (!petSpecs.contains("cat"))
-                        break;
-                    else
-                        petSpecs.remove("cat");
-                }
-                break;
-            case R.id.dog:
-                if (checked)
-                    petSpecs.add("dog");
-                else {
-                    if (!petSpecs.contains("dog"))
-                        break;
-                    else
-                        petSpecs.remove("dog");
-                }
-                break;
-            case R.id.horse:
-                if (checked)
-                    petSpecs.add("horse");
-                else {
-                    if (!petSpecs.contains("horse"))
-                        break;
-                    else
-                        petSpecs.remove("horse");
-                }
-                break;
-            case R.id.pig:
-                if (checked)
-                    petSpecs.add("horse");
-                else {
-                    if (!petSpecs.contains("horse"))
-                        break;
-                    else
-                        petSpecs.remove("horse");
-                }
-                break;
-            case R.id.reptile:
-                if (checked)
-                    petSpecs.add("reptile");
-                else {
-                    if (!petSpecs.contains("reptile"))
-                        break;
-                    else
-                        petSpecs.remove("reptile");
-                }
-                break;
-            case R.id.small_furry:
-                if (checked)
-                    petSpecs.add("small furry");
-                else {
-                    if (!petSpecs.contains("small furry"))
-                        break;
-                    else
-                        petSpecs.remove("small furry");
-                }
-                break;
-            case R.id.male:
-                if (checked)
-                    petSpecs.add("male");
-                else {
-                    if (!petSpecs.contains("male"))
-                        break;
-                    else
-                        petSpecs.remove("male");
-                }
-                break;
-            case R.id.female:
-                if (checked)
-                    petSpecs.add("female");
-                else {
-                    if (!petSpecs.contains("female"))
-                        break;
-                    else
-                        petSpecs.remove("female");
-                }
-                break;
-            case R.id.baby:
-                if (checked)
-                    petSpecs.add("baby");
-                else {
-                    if (!petSpecs.contains("baby"))
-                        break;
-                    else
-                        petSpecs.remove("baby");
-                }
-                break;
-            case R.id.young:
-                if (checked)
-                    petSpecs.add("young");
-                else {
-                    if (!petSpecs.contains("young"))
-                        break;
-                    else
-                        petSpecs.remove("young");
-                }
-                break;
-            case R.id.adult:
-                if (checked)
-                    petSpecs.add("adult");
-                else {
-                    if (!petSpecs.contains("adult"))
-                        break;
-                    else
-                        petSpecs.remove("adult");
-                }
-                break;
-            case R.id.senior:
-                if (checked)
-                    petSpecs.add("senior");
-                else {
-                    if (!petSpecs.contains("senior"))
-                        break;
-                    else
-                        petSpecs.remove("senior");
-                }
-                break;
-            case R.id.small:
-                if (checked)
-                    petSpecs.add("small");
-                else {
-                    if (!petSpecs.contains("small"))
-                        break;
-                    else
-                        petSpecs.remove("small");
-                }
-                break;
-            case R.id.medium:
-                if (checked)
-                    petSpecs.add("medium");
-                else {
-                    if (!petSpecs.contains("medium"))
-                        break;
-                    else
-                        petSpecs.remove("medium");
-                }
-                break;
-            case R.id.large:
-                if (checked)
-                    petSpecs.add("large");
-                else {
-                    if (!petSpecs.contains("large"))
-                        break;
-                    else
-                        petSpecs.remove("large");
-                }
-                break;
-            case R.id.extra_large:
-                if (checked)
-                    petSpecs.add("extra large");
-                else {
-                    if (!petSpecs.contains("extra large"))
-                        break;
-                    else
-                        petSpecs.remove("extra large");
-                }
-                break;
+        CheckBox cb = (CheckBox) findViewById(R.id.barnyard);
+        if (cb.isChecked()) {
+            petSpecs.add("barnyard");
         }
+        cb = (CheckBox) findViewById(R.id.bird);
+        if (cb.isChecked()) {
+            petSpecs.add("bird");
+        }
+        cb = (CheckBox) findViewById(R.id.bird);
+        if (cb.isChecked()) {
+            petSpecs.add("bird");
+        }
+        cb = (CheckBox) findViewById(R.id.cat);
+        if (cb.isChecked()) {
+            petSpecs.add("cat");
+        }
+        cb = (CheckBox) findViewById(R.id.horse);
+        if (cb.isChecked()) {
+            petSpecs.add("horse");
+        }
+        cb = (CheckBox) findViewById(R.id.dog);
+        if (cb.isChecked()) {
+            petSpecs.add("dog");
+        }
+        cb = (CheckBox) findViewById(R.id.pig);
+        if (cb.isChecked()) {
+            petSpecs.add("pig");
+        }
+        cb = (CheckBox) findViewById(R.id.reptile);
+        if (cb.isChecked()) {
+            petSpecs.add("reptile");
+        }
+        cb = (CheckBox) findViewById(R.id.small_furry);
+        if (cb.isChecked()) {
+            petSpecs.add("smallfurry");
+        }
+        cb = (CheckBox) findViewById(R.id.male);
+        if (cb.isChecked()) {
+            petSpecs.add("male");
+        }
+        cb = (CheckBox) findViewById(R.id.female);
+        if (cb.isChecked()) {
+            petSpecs.add("female");
+        }
+        cb = (CheckBox) findViewById(R.id.baby);
+        if (cb.isChecked()) {
+            petSpecs.add("baby");
+        }
+        cb = (CheckBox) findViewById(R.id.young);
+        if (cb.isChecked()) {
+            petSpecs.add("young");
+        }
+        cb = (CheckBox) findViewById(R.id.adult);
+        if (cb.isChecked()) {
+            petSpecs.add("adult");
+        }
+        cb = (CheckBox) findViewById(R.id.senior);
+        if (cb.isChecked()) {
+            petSpecs.add("senior");
+        }
+        cb = (CheckBox) findViewById(R.id.small);
+        if (cb.isChecked()) {
+            petSpecs.add("small");
+        }
+        cb = (CheckBox) findViewById(R.id.medium);
+        if (cb.isChecked()) {
+            petSpecs.add("medium");
+        }
+        cb = (CheckBox) findViewById(R.id.large);
+        if (cb.isChecked()) {
+            petSpecs.add("large");
+        }
+        cb = (CheckBox) findViewById(R.id.extra_large);
+        if (cb.isChecked()) {
+            petSpecs.add("extra-large");
+        }
+
+        Intent intent = getIntent();
+        final String location = intent.getStringExtra("location");
+        petSpecs.add(location);
+        startAysnc(location); // change this to take an ArrayList<String> -> checkSpecs
+        intent = new Intent(PetTypeActivity.this, PetListActivity.class);
+        startActivity(intent);
+
         return petSpecs;
-=======
-//    public void onCheckboxClicked(View view) {
-//        // Is the view now checked?
-//        boolean checked = ((CheckBox) view).isChecked();
-//
-//        for (int i = 0; i < getResources().getStringArray(R.array.animal_specs).length - 1; i++) {
-//
-//        }
-//        //for ()
-//
-//        // Check which checkbox was clicked
-//        switch(view.getId()) {
-//            case R.id.barnyard:
-//                if (checked)
-//                // Put some meat on the sandwich
-//                //else
-//                // Remove the meat
-//                break;
-//            //case R.id.checkbox_cheese:
-//                if (checked)
-//                // Cheese me
-//                //else
-//                // I'm lactose intolerant
-//                break;
-//            // TODO: Veggie sandwich
-//
-//        }
->>>>>>> 1f08b3aa14f7b83b679f68c2f5f6096a99c9f362
+
     }
 
 }
