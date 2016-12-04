aaf7445a8c67453d6596945d609a012670a1bfcb
DAN\dpivonka <dpivonka@comcast.net>
2016-12-04 02:17:16 -0500

    horizontial scrollable image viewer for petview activity

--- app/src/main/java/com/uml/dpivonka/petadoption/PetViewActivity.java
+++ app/src/main/java/com/uml/dpivonka/petadoption/PetViewActivity.java
@@ -3,6 +3,10 @@ package com.uml.dpivonka.petadoption;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * Created by dpivonka on 12/4/2016.
@@ -16,6 +20,14 @@ public class PetViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pet_view);
         Intent intent = getIntent();
         pet = intent.getParcelableExtra("pet");
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.LinearLayout1);
+        for(int i=0; i<pet.getPhotoUrl().size() ; i++)
+        {
+            ImageView ii= new ImageView(this);
+            ImageLoader.getInstance().displayImage(pet.getPhotoUrl().get(i), ii);
+            ll.addView(ii);
+        }
 
 
     }
